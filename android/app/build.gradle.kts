69a4c0e2bc6c8b3cc2d798c919aca5d7c6ed5ef7
iSkyJIE <79014909+iSkyJIE@users.noreply.github.com>
2026-09-11 03:47:05 -1000

    Configure Android app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.iskyjie.duolikeanimation"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.iskyjie.duolikeanimation"
+        minSdk = 33
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
