9d62809caf1ef8e1f7413372ed05d45003b2ac9f
iSkyJIE <79014909+iSkyJIE@users.noreply.github.com>
2026-09-11 04:20:18 -1000

    Use static custom Canvas test view

--- android/app/src/main/java/com/iskyjie/duolikeanimation/MainActivity.java
+++ android/app/src/main/java/com/iskyjie/duolikeanimation/MainActivity.java
@@ -1,24 +1,12 @@
 package com.iskyjie.duolikeanimation;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView text = new TextView(this);
-        text.setText("DuoLikeAnimation\nSAFE BOOT OK");
-        text.setTextSize(24f);
-        text.setTextColor(Color.BLACK);
-        text.setBackgroundColor(Color.WHITE);
-        text.setGravity(Gravity.CENTER);
-        text.setPadding(48, 48, 48, 48);
-
-        setContentView(text);
+        setContentView(new FoldView(this));
     }
 }
